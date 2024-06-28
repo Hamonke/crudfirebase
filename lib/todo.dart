899b42f294bd9bf9ab2d0b13dcc932cb5a7d8b26
Hamonke <152448133+Hamonke@users.noreply.github.com>
2024-06-28 20:16:18 +1200

    deleted riverpods add comments. Need to sort sign in and out and sync.

--- lib/todo.dart
+++ lib/todo.dart
@@ -182,47 +182,5 @@ Future<void> edit({required String id, required String description}) async {
          const Todo(id: 'todo-1', description: 'Star Riverpod'),
          const Todo(id: 'todo-2', description: 'Have a walk'),
        ];
-
-  // void add(String description) {
-  //   state = [
-  //     ...state,
-  //     Todo(
-  //       id: _uuid.v4(),
-  //       description: description,
-  //     ),
-  //   ];
-  // }
-
-  // void toggle(String id) {
-  //   state = [
-  //     for (final todo in state)
-  //       if (todo.id == id)
-  //         Todo(
-  //           id: todo.id,
-  //           completed: !todo.completed,
-  //           description: todo.description,
-  //         )
-  //       else
-  //         todo,
-  //   ];
-  // }
-
-  // void edit({required String id, required String description}) {
-  //   state = [
-  //     for (final todo in state)
-  //       if (todo.id == id)
-  //         Todo(
-  //           id: todo.id,
-  //           completed: todo.completed,
-  //           description: description,
-  //         )
-  //       else
-  //         todo,
-  //   ];
-  // }
-
-  // void remove(Todo target) {
-  //   state = state.where((todo) => todo.id != target.id).toList();
-  // }
-
 }
+
